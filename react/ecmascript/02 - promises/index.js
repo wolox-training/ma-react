6ea4d49e6bd5097b70df92eb6c6466d523873875
Ariel Atar <ari.atar1997@gmail.com>
2018-08-15 15:49:56 -0300

    test 2 promises passed

--- react/ecmascript/02 - promises/index.js
+++ react/ecmascript/02 - promises/index.js
@@ -1,9 +1,13 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
 export function delay(time) {
-
-  return new Promise((resolve) => {
-    resolve(time + 100);
+  return new Promise((resolve, reject) => {
+    if(time < 3000) {
+      resolve(time + 100);
+    } else {
+        let reason = new Error('This time is too much !');
+        reject(reason); // reject
+    }
   });
 }
 
