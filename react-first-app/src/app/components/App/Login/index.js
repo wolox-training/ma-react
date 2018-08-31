7f9842ed34c38c80dc7abda0787bd4edcdf20f35
Ariel Atar <ari.atar1997@gmail.com>
2018-08-31 13:00:32 -0300

    created db.json and connected to the API doing GETs and POSTs

--- react-first-app/src/app/components/App/Login/index.js
+++ react-first-app/src/app/components/App/Login/index.js
@@ -5,16 +5,29 @@ import { createStore, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import RegisterForm from './login.js';
 import { SubmissionError } from 'redux-form';
+import {create} from 'apisauce';
 
 class LoginContainer extends React.Component {
   submit = values => {
-    if (['kent@gmail.com', 'andy@gmail.com', 'john@gmail.com', 'joel@gmail.com'].includes(values.email)) {
+    // define the api
+    const api = create({
+      baseURL: 'http://localhost:4000',
+      headers: {'Accept': 'application/vnd.github.v3+json'}
+    })
+
+    // start making calls
+    /*api
+      .get('/users')
+      .then((response) => response.data[0].name)
+      .then(console.log)*/
+    api.post('/users', {id: 2, name: 'Steve', email: 'steve@gmail.com'}, {headers: {'x-gigawatts': '1.21'}})
+    /*if (['kent@gmail.com', 'andy@gmail.com', 'john@gmail.com', 'joel@gmail.com'].includes(values.email)) {
       window.alert(JSON.stringify(values, null, 4));
     } else {
       throw new SubmissionError({
         email: 'Email not registered'
       });
-    }
+    }*/
   };
 
   render() {
