4687a6829d8f1aedfe5d0316a51c37bb480ad4a6
Ariel Atar <ari.atar1997@gmail.com>
2018-08-31 11:31:34 -0300

    validating valid email and password length

--- react-first-app/src/app/components/App/Login/fields/index.js
+++ react-first-app/src/app/components/App/Login/fields/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 
 export const customInput = props => {
+  const { label, input, type, meta } = props;
   return (
     <div>
-      <label>{props.label}</label>
-      <input {...props.input} type={props.type} />
-    </div>
-  );
-};
-
-export const customSelect = props => {
-  return (
-    <div>
-      <label>{props.label}</label>
-      <select {...props.input}>
-        <option />
-        <option value="tabs">Tabs</option>
-        <option value="spaces">Spaces</option>
-      </select>
+      <label>{label}</label>
+      <input {...input} type={type} />
+      {meta.error &&
+        meta.touched && (
+          <div style={{ color: 'red' }}>{meta.error}</div>
+        )}
     </div>
   );
 };
