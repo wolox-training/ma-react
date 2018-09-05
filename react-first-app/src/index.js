fe3f0053145b7190ea6c90bf222463edbfa9bfd5
Ariel Atar <ari.atar1997@gmail.com>
2018-09-05 13:22:52 -0300

    starting with topbar

--- react-first-app/src/index.js
+++ react-first-app/src/index.js
@@ -9,12 +9,23 @@ import Game from '~components/App/Game';
 
 import './scss/index.scss';
 import registerServiceWorker from './registerServiceWorker';
-import store from './redux/store.js';
+import store from './redux/store';
+import Game from '~components/App/Game';
+import LoginContainer from '~components/App/Login/index.js';
+import './styles.css';
+
+const NavBar = () => (
+  <header className="navbar">
+      <img src="wolox_logo.svg" alt="Wolox" class="wolox-logo" />
+      <span className="nav-text-right">Developer tools</span>
+  </header>
+)
 
 const App = () => (
   <Provider store={store}>
     <Router>
       <div>
+        <NavBar />
         <Route exact path="/" component={Game} />
         <Route path="/login" component={LoginContainer} />
       </div>
